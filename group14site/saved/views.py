79c6cff541ec242eec07fa98460470ae5ad3fe76
miaar <miaark952@gmail.com>
2025-04-07 18:12:36 -0400

    comments feature finished

--- group14site/saved/views.py
+++ group14site/saved/views.py
@@ -13,7 +13,8 @@ def index(request):
 
 def see_recommendation_details(request, rec_id):
    details = get_rec_details(rec_id)
-   return render(request, 'saved_rec_details.html', {'rec': details,})
+   comments = get_comments(rec_id)
+   return render(request, 'saved_rec_details.html', {'rec': details, 'comments': comments})
 
 #NOTE: right now I cannot filter saved recs to a specific user id because I can't login and save user id state
 def get_saved_recs():
@@ -31,11 +32,15 @@ def query_db(query, rec_id):
    return dict(zip(cols,row)) if row else None
 
 def get_rec_details(rec_id):
-   details = query_db("SELECT * FROM savedrecommendations JOIN users ON user_posted_id = user_id NATURAL JOIN recommendationpost NATURAL JOIN recommendeditem NATURAL JOIN groups WHERE recommendation_id = %s", rec_id)
-   return details
+   with connection.cursor() as c:
+      c.execute("SELECT * FROM savedrecommendations JOIN users ON user_posted_id = user_id NATURAL JOIN recommendationpost NATURAL JOIN recommendeditem NATURAL JOIN groups WHERE recommendation_id = %s", [rec_id])
+      cols = [col[0] for col in c.description]
+      row = c.fetchone()
+   return dict(zip(cols, row)) if row else None
 
 def get_comments(rec_id):
-   comments = query_db("SELECT * FROM comment NATURAL JOIN users WHERE recommendation_id = %s",rec_id)
+   with connection.cursor() as c:
+      c.execute("SELECT * FROM comment JOIN users ON comment.user_id = users.user_id WHERE recommendation_post_id = %s",[rec_id])
+      cols = [col[0] for col in c.description]
+      comments =  [dict(zip(cols, row)) for row in c.fetchall()]
    return comments
-
-
