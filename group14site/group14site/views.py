a43b3a0e9cca48931883127a3e1aa2b40ea9b2a0
anjalipagidi <97760542+anjalipagidi@users.noreply.github.com>
2025-04-08 19:22:03 -0400

    clean up view files

--- group14site/group14site/views.py
+++ group14site/group14site/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.db import connection
 
 
@@ -7,19 +7,18 @@ def index(request):
     success_message = ""
 
     if request.method == "POST":
-        if "signup" in request.POST:
-            username = request.POST.get("username")
-            password = request.POST.get("password")
+        username = request.POST.get("username")
+        password = request.POST.get("password")
 
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT user_id FROM Users WHERE username = %s", [username])
+            existing_user = cursor.fetchone()
+
+        if existing_user:
+            error_message = "Username already exists."
+        else:
             with connection.cursor() as cursor:
-                cursor.execute("SELECT user_id FROM Users WHERE username = %s", [username])
-                existing_user = cursor.fetchone()
-
-            if existing_user:
-                error_message = "Username already exists."
-            else:
-                with connection.cursor() as cursor:
-                    cursor.execute("INSERT INTO Users (username, password) VALUES (%s, %s)", [username, password])
-                success_message = "User created successfully. You can now log in."
+                cursor.execute("INSERT INTO Users (username, password) VALUES (%s, %s)", [username, password])
+            success_message = "User created successfully. You can now log in."
 
     return render(request, "group14site/index.html", {"error_message": error_message, "success_message": success_message})
