cc7ec424d9c1df357f0f52a386d5af43fbe9f151
miaar <miaark952@gmail.com>
2025-04-10 19:17:19 -0400

    user id for browse groups

--- group14site/group14site/settings.py
+++ group14site/group14site/settings.py
@@ -11,6 +11,16 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_NAME = os.getenv('DB_NAME')
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_HOST =  os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,11 +93,11 @@ WSGI_APPLICATION = 'group14site.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "group14",
-        "USER": "group14",
-        "PASSWORD": "I4yeI1U1",
-        "HOST": "bastion.cs.virginia.edu",
-        "PORT": "5432",
+        "NAME": DB_NAME,
+        "USER": DB_USER,
+        "PASSWORD": DB_PASSWORD,
+        "HOST": DB_HOST,
+        "PORT": DB_PORT,
     }
 }
 
