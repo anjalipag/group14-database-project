787c57ae343d9f979cb9ebb82eb49ff96408d10d
miaar <miaark952@gmail.com>
2025-04-20 20:45:53 -0400

    Changed savedrec details page to same one in groupfeed for consistency

--- group14site/saved/urls.py
+++ group14site/saved/urls.py
@@ -1,10 +1,7 @@
-from django.urls import path
-
+from django.urls import include, path
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("details/<int:rec_id>/", views.see_recommendation_details, name="saved_rec_details"),
-    path("details/<int:rec_id>/upvote", views.handle_upvote, name="upvote"),
-    path("details/<int:rec_id>/downvote", views.handle_downvote, name="downvote"),
+    path("group/feed/", include("groupFeed.urls"))
 ]
