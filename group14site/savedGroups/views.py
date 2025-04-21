ce97bdcd3b9bc840c65b803a4a9b54e093a22622
miaar <miaark952@gmail.com>
2025-04-21 12:23:35 -0400

    my created groups populated

--- group14site/savedGroups/views.py
+++ group14site/savedGroups/views.py
@@ -5,7 +5,8 @@ def my_groups(request):
     # user_id = request.GET.get('user_id')
     user_id = request.session.get('user_id')
     groups = get_groups_joined_by_user(user_id)
-    return render(request, 'my_groups.html', {'groups': groups})
+    groups_created = get_groups_created(user_id)
+    return render(request, 'my_groups.html', {'groups': groups, 'groups_created': groups_created})
 
 def get_groups_joined_by_user(user_id):
     with connection.cursor() as c:
@@ -17,6 +18,18 @@ def get_groups_joined_by_user(user_id):
             WHERE g.group_id IN (
                 SELECT group_id FROM groupsmembers WHERE user_id = %s
             )
+        """, [user_id])
+        cols = [col[0] for col in c.description]
+        return [dict(zip(cols, row)) for row in c.fetchall()]
+
+def get_groups_created(user_id):
+    with connection.cursor() as c:
+        c.execute("""
+            SELECT g.group_id, g.group_name, g.group_photo,
+                   g.admin_id AS admin_username
+            FROM groups g
+            JOIN users u ON g.admin_id = u.user_id
+            WHERE g.admin_id = %s
         """, [user_id])
         cols = [col[0] for col in c.description]
         return [dict(zip(cols, row)) for row in c.fetchall()]
