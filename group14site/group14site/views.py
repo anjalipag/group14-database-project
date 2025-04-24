095f4fb480c229546a6d33c1cd0c9747a96d5d6d
miaar <miaark952@gmail.com>
2025-04-23 21:41:41 -0400

    hashed pass

--- group14site/group14site/views.py
+++ group14site/group14site/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.db import connection
+from django.contrib.auth.hashers import make_password
 
 
 def index(request):
@@ -12,6 +13,7 @@ def index(request):
     if request.method == "POST":
         username = request.POST.get("username")
         password = request.POST.get("password")
+        hashed_password = make_password(password)
 
         with connection.cursor() as cursor:
             cursor.execute("SELECT user_id FROM Users WHERE username = %s", [username])
@@ -21,7 +23,7 @@ def index(request):
             error_message = "Username already exists."
         else:
             with connection.cursor() as cursor:
-                cursor.execute("INSERT INTO Users (username, password) VALUES (%s, %s)", [username, password])
+                cursor.execute("INSERT INTO Users (username, password) VALUES (%s, %s)", [username, hashed_password])
             success_message = "User created successfully. You can now log in."
 
     return render(request, "group14site/index.html", {"error_message": error_message, "success_message": success_message})
--- group14site/login/views.py
+++ group14site/login/views.py
@@ -1,22 +1,26 @@
 from django.shortcuts import render, redirect
 from django.db import connection
+from django.contrib.auth.hashers import check_password
 
+#How to hash passwords Source Used: https://docs.djangoproject.com/en/5.2/topics/auth/passwords/
 
 def index(request):
-    error_message = ""
-    if request.method == "POST":
-        username = request.POST.get("username")
-        password = request.POST.get("password")
+  error_message = ""
+  if request.method == "POST":
+      username = request.POST.get("username")
+      password = request.POST.get("password")
 
-        with connection.cursor() as cursor:
-            cursor.execute("SELECT * FROM Users WHERE username = %s AND password = %s", [username, password])
-            user = cursor.fetchone()
+      with connection.cursor() as cursor:
+          cursor.execute("SELECT user_id, password FROM Users WHERE username = %s", [username])
+          user = cursor.fetchone()
 
-        if user:
-            user_id = user[0]
-            request.session["user_id"] = user_id
-            return redirect(f"/group/saved/")
-        else:
-            error_message = "Invalid username or password."
+      if user and check_password(password, user[1]):
+          user_id = user[0]
+          request.session["user_id"] = user_id
+          return redirect(f"/group/saved/")
+      else:
+          error_message = "Invalid username or password."
 
-    return render(request, "login/index.html", {"error_message": error_message})
+  return render(request, "login/index.html", {"error_message": error_message})
+
+
